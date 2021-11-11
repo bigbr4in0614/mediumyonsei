56ea06763847520028b518dd26722454c47a2dfd
Bigbr4in0614 <bigbr4in0614@gmail.com>
2021-11-11 14:40:52 +0900

    temporarily added about page

---
+++ src/components/Topnav.js
@@ -0,0 +1,19 @@
+import './style.css';
+import mainlogo from '../assets/logo/big/white.png';
+
+function Topnav() {
+  return (
+    <div className="topnav">
+      <img class="logo" src={mainlogo} />
+        <div class="navgroup">
+          <a class="active"> About </a>
+          <a href="option.html"> Instruction</a>
+          <a href="playground.html">Playground</a>
+          <a href="https://github.com/bigbr4in0614/hate-speech-detection" target="_blank">Github</a>
+      </div>
+    </div>
+  );
+}
+
+
+export default Topnav;
