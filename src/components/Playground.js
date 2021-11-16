9d8f0b201428e35ab2b380682f7108c62635fc67
Bigbr4in0614 <bigbr4in0614@gmail.com>
2021-11-16 17:18:41 +0900

    added description to playground

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -5,18 +5,56 @@ import submit from "./Rocketll";
 import { useState } from "react";
 
 function Playground() {
-    const [userInput, setUserInput] = useState('')
-    const [userOutput, setUserOutput] = useState([null, null])
+  const [userInput, setUserInput] = useState("");
+  const [userOutput, setUserOutput] = useState([null, null]);
   return (
     <div>
       <Topnav />
       <div class="contentbox">
+        <h1>
+          Playground
+        </h1>
+        <p className="playinstruction">
+          두 단어 이상의 문장을 입력하고 Submit 버튼을 누르면 문장 단위로 
+          <span className="hatelabel"> 혐오 표현</span>/ <span className="offlabel">공격적 표현</span>/일반적 표현 으로
+          구별되어 아래의 박스에 표시됩니다.
+        </p>
         <div class="boxcontainer">
-          <textarea class="inputbox" id="input" onChange={(event) => {setUserInput(event.target.value)}}></textarea>
-          <button class="submitbutton" id="submit" onClick={() => {submit(userInput).then((val) => {setUserOutput(val); console.log(val)})}}>
+          <textarea
+            class="inputbox"
+            id="input"
+            onChange={(event) => {
+              setUserInput(event.target.value);
+            }}
+          ></textarea>
+          <button
+            class="submitbutton"
+            id="submit"
+            onClick={() => {
+              submit(userInput).then((val) => {
+                setUserOutput(val);
+                console.log(val);
+              });
+            }}
+          >
             submit
           </button>
-          <div class="outputbox" id="output" readOnly>{userOutput[1]?.map((val,i) => <span style={ {color: userOutput[0][0][i]?.label==="offensive" ? "orange" : userOutput[0][0][i]?.label==="hate" ? "red" : "inherit"  } }>{val} / </span> )}</div>
+          <div class="outputbox" id="output" readOnly>
+            {userOutput[1]?.map((val, i) => (
+              <span
+                style={{
+                  color:
+                    userOutput[0][0][i]?.label === "offensive"
+                      ? "orange"
+                      : userOutput[0][0][i]?.label === "hate"
+                      ? "red"
+                      : "inherit",
+                }}
+              >
+                {val}
+              </span>
+            ))}
+          </div>
         </div>
       </div>
     </div>
